380f382b469a269138844e6063f41be84c4f751a
Robert Medvedec <robert.medvedec@microblink.com>
2025-02-03 15:11:11 +0100

    Change verification service url

--- BlinkIDVerifySample/lib-common/src/main/java/com/microblink/blinkidverify/sample/config/Config.kt
+++ BlinkIDVerifySample/lib-common/src/main/java/com/microblink/blinkidverify/sample/config/Config.kt
@@ -3,6 +3,6 @@ package com.microblink.blinkidverify.sample.config
 object BlinkIDVerifyConfig {
     const val licenseKey =
         "sRwDAAEjY29tLm1pY3JvYmxpbmsuYmxpbmtpZHZlcmlmeS5zYW1wbGUACm1pY3JvYmxpbmskwZmPqOWkBJRBRUnMQ0dugDtClptbCGwy+mpK4xn4Zrvy8jbdUR/o1pWtxjzs98PNtrOgbx6mKov9ifpmTVzdLMfVZoAEwBOpKjO8ZW+3iUgxNPM+1l6SoM9NypHMPXL9eaVpCu/sIJNnVGd56w=="
-    const val verificationServiceBaseUrl = "https://usc1.verify.microblink.com/api/v2/docver"
+    const val verificationServiceBaseUrl = "https://us-east.verify.microblink.com/api/v2"
     const val verificationServiceToken =  "<your_api_token>"
 }
